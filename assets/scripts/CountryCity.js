8d00925dca3f5165bee16fa377d6f7e2ec46efc7
Bhupen Pal <bhupen16pal@gmail.com>
2020-07-07 18:20:54 +0530

    Rendering cities based on countries

---
+++ assets/scripts/CountryCity.js
@@ -0,0 +1,38 @@
+const CountryCity = [
+  {
+    Country: "INDIA",
+    City: [
+      "146",
+      "147",
+      "155",
+      "156",
+      "159",
+      "166",
+      "33",
+      "4C",
+      "75",
+      "Giulia",
+      "Giulietta",
+      "GT",
+      "GTV",
+      "Mito",
+      "Spider",
+      "Sprint",
+      "Stelvio",
+      "OTHER"
+    ],
+  },
+  {
+    Country: "ASTON MARTIN",
+    City: [
+      "DV7",
+      "DB9",
+      "DBS",
+      "Rapide",
+      "V8 Vantage",
+      "Vanquish",
+      "Vantage",
+      "Virage"
+    ],
+  }
+]
